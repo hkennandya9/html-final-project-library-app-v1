dbc4d269de754734bb83da24dbf17c22964744b4
hkennandya9 <hkennandya@gmail.com>
2023-05-21 19:05:34 +0700

    init

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Hafizh Kennandya Maulana'; // TODO: replace this
+
+const studentId = 'FE4593655'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
